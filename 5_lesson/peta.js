292eac1b2059e9b0dc9579200e64334582e8db5d
User <lejlagorcevic02@gmail.com>
2022-12-01 20:31:00 +0100

    JS

---
+++ 5_lesson/peta.js
@@ -0,0 +1,20 @@
+// var a = 10
+// let b = 20
+// const c = 30
+// d = 5
+
+// HOISTING //
+// Hoisting predstavlja deklarisanje promenljivih pre same dodele vrednosti
+// (u pozadini).
+// Prilikom čitanja koda se "izvlači" deklaracija na vrhu.
+
+prom1 = 23;
+console.log(prom1);
+
+// var prom1; možemo izvršiti naknadnu deklaraciju već korišćene promenljive preko var keyword
+// (a i ne mora, jer JavaScript to radi u pozadini prilikom čitanja koda)
+
+// let prom1; kasnije deklarisanje promenljive reko let keyword nije moguće
+
+// const prom1; kasnije deklarisanje promenljive preko const keyword nije moguće
+
