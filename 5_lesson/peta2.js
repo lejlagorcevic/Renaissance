3f882bdda7d06b300c26fd1ed988a72c38127d8b
User <lejlagorcevic02@gmail.com>
2022-12-02 23:56:16 +0100

    JS

--- 5_lesson/peta2.js
+++ 5_lesson/peta2.js
@@ -15,7 +15,14 @@ console.log(recenica2.length);
 
 const novaRecenica = "Spava mi se."
 const char1 = novaRecenica.charAt(3);
+const char11 = novaRecenica.charAt(46); //prazan string
 console.log(char1);
 
 const char2 = novaRecenica.charCodeAt(3);
 console.log(char2);
+
+const char3 = novaRecenica[3];
+const char33 = novaRecenica[32]; //rezultat ce biti undefined
+console.log(char3);
+console.log(char11);
+console.log(char33);
---
+++ 5_lesson_hw/homework.js
@@ -0,0 +1,15 @@
+// DOMAĆI //
+// Zbir cifara četvorocifrenog broja
+// Napiši program koji učitava četvorocifreni broj i ispisuje zbir svih cifara tog broja.
+// Ulaz: U jednoj liniji standardnog ulaza nalazi se četvorocifreni broj.
+// Izlaz: Zbir cifara četvorocifrenog broja.
+// Primer
+// Ulaz 1234
+// Izlaz 10
+function sumNumbers(){
+    num1(Number) = document.getElementById("number1").value;
+    num2(Number) = document.getElementById("number2").value;
+    num3(Number) = document.getElementById("number3").value;
+    num4(Number) = document.getElementById("number4").value;
+    document.getElementById("result").innerHTML = num1(Number) + num2(Number) + num3(Number) + num4(Number);
+}
