b3b4cba4c9446846599bd9bffaeb8f7e464e5978
User <lejlagorcevic02@gmail.com>
2022-12-04 23:49:15 +0100

    JS

---
+++ 5_lesson_hw/hw.js
@@ -0,0 +1,15 @@
+// DOMAĆI //
+// Zbir cifara četvorocifrenog broja
+// Napiši program koji učitava četvorocifreni broj i ispisuje zbir svih cifara tog broja.
+// Ulaz: U jednoj liniji standardnog ulaza nalazi se četvorocifreni broj.
+// Izlaz: Zbir cifara četvorocifrenog broja.
+// Primer
+// Ulaz 1234
+// Izlaz 10
+function sumNumbers(){
+    num1 = document.getElementById("number1").value;
+    num2 = document.getElementById("number2").value;
+    num3 = document.getElementById("number3").value;
+    num4 = document.getElementById("number4").value;
+    document.getElementById("result").innerHTML = num1(Number) + num2(Number) + num3(Number) + num4(Number);
+}git add 
