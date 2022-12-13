f73186a39ef63b09fe8ad248fd139804d80b327c
User <lejlagorcevic02@gmail.com>
2022-12-13 21:30:58 +0100

    JS

--- 7_lesson/app.js
+++ 7_lesson/app.js
@@ -26,3 +26,44 @@ console.log("10"-2); //"8"
 
 // Za proveru da li je neka vrednost korektan broj se kosirsti !isNaN().
 console.log(isNaN(100/"Apple"));
+
+const a = 25;
+const c = -1
+const b = 0;
+
+console.log(a/b); 
+console.log(c/b);
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+// rgb(0, 255, 123)
+// #12ab33
+// rgb(18, 171, 51)
+// 12 = 1*16 + 2*1 = 18
+// ab = 10*16 + 11*1 = 17
+// 33 = 3*16 + 3*1 = 48 + 3 = 51
+
+// Hexadecimal numbers:
+// Ako su brojevi napisani prefiksom 0xFF rezultat će biti 255.
+const r = 0xff;
+console.log(r);
+
+
+// Pored onsovnog naičina dodeljivanja vrednosti nekoj promenljivoj možemo to uraditi i preko new:
+const k = 22;
+const l = new Number(33);
+
+console.log(k);
+console.log(l);
+
+console.log(k + l);
+
+console.log(typeof k);
+console.log(typeof l);
+
+// Napraviti funkciju koja: 
+// a) pretvara km u m
+// b) prtvara inch u cm
+
+function
